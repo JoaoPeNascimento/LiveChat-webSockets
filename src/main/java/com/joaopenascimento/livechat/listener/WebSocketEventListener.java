90a04c92ab93cb2840d6057dcc5a79881506bdd5
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-12-15 21:04:11 -0300

    fix notification

--- src/main/java/com/joaopenascimento/livechat/listener/WebSocketEventListener.java
+++ src/main/java/com/joaopenascimento/livechat/listener/WebSocketEventListener.java
@@ -1,20 +1,19 @@
-package com.joaopenascimento.livechat;
+package com.joaopenascimento.livechat.listener;
 
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.security.Principal;
 
 @Component
 public class WebSocketEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
-
     private final SimpMessagingTemplate messagingTemplate;
 
     public WebSocketEventListener(SimpMessagingTemplate messagingTemplate) {
@@ -23,14 +22,11 @@ public class WebSocketEventListener {
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        Principal user = event.getUser();
 
-        String username = headerAccessor.getFirstNativeHeader("username");
-
-        if(username != null) {
+        if (user != null) {
+            String username = user.getName();
             logger.info("Novo usuário conectado: " + username);
-
-            headerAccessor.getSessionAttributes().put("username", username);
 
             messagingTemplate.convertAndSend("/topics/status", username + " entrou no chat.");
         }
@@ -38,12 +34,12 @@ public class WebSocketEventListener {
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        Principal user = event.getUser();
 
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
-
-        if (username != null) {
+        if (user != null) {
+            String username = user.getName();
             logger.info("Usuário desconectado: " + username);
+
             messagingTemplate.convertAndSend("/topics/status", username + " saiu do chat.");
         }
     }
